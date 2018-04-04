4d847581907cc80279a5c2fcbbc4b37ed8a886c9
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-04-04 02:28:22 -0300

    Funtion to acess JSON file

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var scheduleMaker = angular.module("scheduleMakerApp", []);
 
-scheduleMaker.controller('controller', function($scope){
+scheduleMaker.controller('controller', function($scope, $http){
 	$scope.title = "Schedule Maker";
+	var horarios;
+	getHorarios();
+
+	function getHorarios () {
+		$http.get('http://127.0.0.1:8000/assets/horarios.json')
+		.then(function successCallback(data) {
+			horarios = data;
+			console.log(horarios);
+		}, function (error) {
+			console.log(error);
+		});
+	}
 });
